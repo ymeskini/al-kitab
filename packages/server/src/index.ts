b9f11c4db572bb8d1760a055946719967fcb714a
Youssef <youss.mesini@gmail.com>
2019-12-09 17:04:28 +0100

    Security for the server

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,21 +1,42 @@
-const { ApolloServer, gql } = require('apollo-server');
+import express from 'express';
+import { ApolloServer, gql } from 'apollo-server-express';
+import helmet from 'helmet';
+import compression from 'compression';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
 
-// The GraphQL schema
+const origin = {
+  origin: '*' /* in prod get the url of front and mobile */,
+};
+
+// Construct a schema, using GraphQL schema language
 const typeDefs = gql`
   type Query {
     hello: String
   }
 `;
 
-// A map of functions which return data for the schema.
+// Provide resolver functions for your schema fields
 const resolvers = {
   Query: {
-    hello: () => 'world',
+    hello: () => 'Hello world!',
   },
 };
 
 const server = new ApolloServer({ typeDefs, resolvers });
 
-server.listen().then(() => {
-  console.log(`🚀  Server ready at localhost:4000`);
-});
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(helmet());
+app.use(compression());
+app.use(cors(origin));
+app.use(morgan('tiny'));
+
+server.applyMiddleware({ app });
+
+app.listen({ port: 4000 }, () =>
+  console.log(`🚀 Server ready at http://localhost:4000${server.graphqlPath}`),
+);
