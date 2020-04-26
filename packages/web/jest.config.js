3cc5fa8a513445f316aff7f5e2d2b3d0619853e6
Youssef Meskini <youss.meskini@gmail.com>
2020-04-26 17:30:49 +0200

    chore(config): adapt web config for tests

--- packages/web/jest.config.js
+++ packages/web/jest.config.js
@@ -1,1 +1,5 @@
-module.exports = {};
+module.exports = {
+  collectCoverageFrom: ['**/*.{js,jsx,ts,tsx}', '!**/*.d.ts', '!**/node_modules/**'],
+  testPathIgnorePatterns: ['/node_modules/', '/.next/'],
+  transformIgnorePatterns: ['/node_modules/'],
+};
